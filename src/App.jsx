151c5b298d9e0d7e9c0ea0eb84a4c6a4f642d74b
Damian Aung <damianaung@Damians-MacBook-Air.local>
2025-01-18 14:28:41 +0700

    project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { Link, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import Menu from "./components/Menu";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
 
 function App() {
 	return (
 		<div>
-			<nav className="flex justify-center space-x-4 p-4 bg-gray-200">
+			<nav className="flex justify-center space-x-4 gap-20 p-4 bg-gray-200">
 				<Link to="/" className="text-blue-600">
 					Home
 				</Link>
 				<Link to="/menu" className="text-blue-600">
-					About
+					Menu
 				</Link>
 			</nav>
 			<Routes>
---
+++ src/components/Menu/create.jsx
@@ -0,0 +1,13 @@
+import { CreateMenuItem } from "./CreateMenuItem";
+
+// Component to create a new menu item
+
+// Main Component
+export default function createMenu() {
+	return (
+		<div>
+			<h1>Shal-Phyoke-POS</h1>
+			<CreateMenuItem />
+		</div>
+	);
+}
