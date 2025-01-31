3363b5ca9e6cb9fd830c1a2091f512e6312e9355
CryoMilo <minkojee@gmail.com>
2025-02-01 04:22:00 +0700

    added realtime subscriptions on delete and create

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -37,21 +37,21 @@ const Order = () => {
 		}
 	};
 
+	const fetchOrders = async () => {
+		const { data, error } = await supabase
+			.from("order")
+			.select("*, table:table_id(*)")
+			.order("created_at", { ascending: true });
+
+		if (error) {
+			console.error("Error fetching orders:", error);
+		} else {
+			setOrders(data);
+		}
+		setLoading(false);
+	};
+
 	useEffect(() => {
-		const fetchOrders = async () => {
-			const { data, error } = await supabase
-				.from("order")
-				.select("*, table:table_id(*)")
-				.order("created_at", { ascending: true });
-
-			if (error) {
-				console.error("Error fetching orders:", error);
-			} else {
-				setOrders(data);
-			}
-			setLoading(false);
-		};
-
 		fetchOrders();
 	}, []);
 
@@ -66,6 +66,22 @@ const Order = () => {
 						prevOrders.map((order) =>
 							order.id === payload.new.id ? { ...order, ...payload.new } : order
 						)
+					);
+				}
+			)
+			.on(
+				"postgres_changes",
+				{ event: "INSERT", schema: "public", table: "order" },
+				(payload) => {
+					setOrders((prevOrders) => [...prevOrders, payload.new]);
+				}
+			)
+			.on(
+				"postgres_changes",
+				{ event: "DELETE", schema: "public", table: "order" },
+				(payload) => {
+					setOrders((prevOrders) =>
+						prevOrders.filter((order) => order.id !== payload.old.id)
 					);
 				}
 			)
